762a7e539b59125e3cc93e3241e675faaf8e73b6
Aleksandr <shunko1999@gmail.com>
2023-06-28 23:04:07 +0300

    feat: add base structure
    add components header, navbar

--- wiks/src/App.tsx
+++ wiks/src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
 <div className='app-wrapper'>
- <header className='header'>
-  <img src="https://w7.pngwing.com/pngs/271/827/png-transparent-computer-icons-bookmark-social-media-blue-logo-social-media.png" alt="favicon" />
- </header>
- <nav className='nav'>
-<div>
-  Profile
-</div>
-<div>Messages</div>
- </nav>
- <div className='content'>
-  Main content
+<Header />
+ <Navbar/>
+ <div className='content '>
+  <div>
+  <img src="https://sportishka.com/uploads/posts/2022-04/1651298115_32-sportishka-com-p-milaya-mashina-mashini-krasivo-foto-38.jpg" alt="auto" />
+  </div>
+
  </div>
 </div>
   );
---
+++ wiks/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+return <header className='header'>
+  <img src="https://w7.pngwing.com/pngs/271/827/png-transparent-computer-icons-bookmark-social-media-blue-logo-social-media.png" alt="favicon" />
+  <div>Head</div>
+ </header>
+}
+
+export default Header;
