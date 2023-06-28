d145d4ecc47ae1ac93ea774fb809d97c5ea5ba05
Aleksandr <shunko1999@gmail.com>
2023-06-28 22:35:23 +0300

    init: install project

---
+++ wiks/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div className='app-wrapper'>
+ <header className='header'>
+  <img src="https://w7.pngwing.com/pngs/271/827/png-transparent-computer-icons-bookmark-social-media-blue-logo-social-media.png" alt="favicon" />
+ </header>
+ <nav className='nav'>
+<div>
+  Profile
+</div>
+<div>Messages</div>
+ </nav>
+ <div className='content'>
+  Main content
+ </div>
+</div>
+  );
+}
+
+export default App;
