0cf631183a408663fcb0fa0948637eec2698171e
Aleksandr <shunko1999@gmail.com>
2023-06-29 22:47:52 +0300

    feat: add component content

--- wiks/src/App.tsx
+++ wiks/src/App.tsx
@@ -3,20 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Content from './components/Content';
+
 
 function App() {
   return (
 <div className='app-wrapper'>
 <Header />
  <Navbar/>
- <div className='content '>
-  <div>
-  <img src="https://sportishka.com/uploads/posts/2022-04/1651298115_32-sportishka-com-p-milaya-mashina-mashini-krasivo-foto-38.jpg" alt="auto" />
-  </div>
-
- </div>
+ <Content/>
 </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ wiks/src/components/Content.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Content = () => {
+    return <div className='content '>
+    <div>
+    <img src="https://sportishka.com/uploads/posts/2022-04/1651298115_32-sportishka-com-p-milaya-mashina-mashini-krasivo-foto-38.jpg" alt="auto" />
+    </div>
+  <div>
+    <div>Text 1</div>
+  </div>
+  <div>
+    <div>Text 2</div>
+  </div>
+  <div>
+    <div>Text 3</div>
+  </div>
+  <div>
+    <div>Text 4</div>
+  </div>
+   </div>
+}
+
+export default Content;
