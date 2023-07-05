4487a2f3ab0cefa41ad6d9e795b5cb4f8180744a
Aleksandr <shunko1999@gmail.com>
2023-07-05 23:36:23 +0300

    feat: create component Dialogs, add styles for Dialogs

--- wiks/src/components/Dialogs/Dialogs.tsx
+++ wiks/src/components/Dialogs/Dialogs.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import styles from './Dialogs.module.css'
-
+import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
-}
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog + ' ' + styles.active}>Andrey</div>
+        <div className={styles.dialog}>Andrey</div>
+        <div className={styles.dialog}>Andrey</div>
+        <div className={styles.dialog}>Andrey</div>
+        <div className={styles.dialog}>Andrey</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>How</div>
+        <div className={styles.message}>You</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
