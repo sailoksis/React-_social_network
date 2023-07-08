9546496be126cf5836b73ca7f1087a560d117e99
Aleksandr <shunko1999@gmail.com>
2023-07-08 20:31:41 +0300

    refact: refactoring components DialogsItem and Message

--- wiks/src/components/Dialogs/Dialogs.tsx
+++ wiks/src/components/Dialogs/Dialogs.tsx
@@ -2,23 +2,34 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
+const DialogsItem = (props: any) => {
+  let path = "/dialogs/" + props.id;
+return <div className={styles.dialog + " " + styles.active}>
+<NavLink to={path}>{props.name}</NavLink>
+</div>
+}
+
+const Message = (props: any) => {
+  return  <div className={styles.dialog}>{props.message}</div>
+        }
+
 const Dialogs = () => {
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>
-        <div className={styles.dialog + " " + styles.active}>
-          <NavLink to="/dialogs/1">Mihail</NavLink>
-        </div>
-
-        <div className={styles.dialog}><NavLink to="/dialogs/2">Ana</NavLink></div>
-        <div className={styles.dialog}><NavLink to="/dialogs/3">Dima</NavLink></div>
-        <div className={styles.dialog}><NavLink to="/dialogs/4">Hell</NavLink></div>
-        <div className={styles.dialog}><NavLink to="/dialogs/5">Andrey</NavLink></div>
+        <DialogsItem name='Mihail' id = '1'/>
+        <DialogsItem name='Anya' id = '2'/>
+        <DialogsItem name='Dima' id = '3'/>
+        <DialogsItem name='Oleg' id = '4'/>
+        <DialogsItem name='Sega' id = '5'/>
+        <DialogsItem name='Sim' id = '6'/>
       </div>
+
+      
       <div className={styles.messages}>
-        <div className={styles.message}>Hi</div>
-        <div className={styles.message}>How</div>
-        <div className={styles.message}>You</div>
+        <Message message = 'Hi'></Message>
+        <Message message = 'Buy'></Message>
+        <Message message = 'Hello '></Message>
       </div>
     </div>
   );
